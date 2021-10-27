4fbb3c0fc0f0c42d5c760f8daf72fd42c9c9976f
Hetu Patel <hetu200211@gmail.com>
2021-10-28 01:33:47 +0530

    loader added

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../../css/loader.css'
-
+import React, { useEffect } from "react";
+import "../../css/loader.css";
+import img from '../../assets/Gif/loader.gif'
 function Loader() {
-    return (
-        <div className="loader_body">
-        <div class="loader">Loading...</div>
-        </div>
-    )
+  return (
+    <div className="loader_body">
+      <img src={img}></img>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
