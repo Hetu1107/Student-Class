fdd38a88718c3a599f7ca5197ebefcc3279a5731
zaid bhimala <zaidm124@gmail.com>
2021-12-19 15:11:12 +0530

    forgot password connected to email js

--- server.js
+++ server.js
@@ -1,80 +1,80 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const server = require('http').createServer(app);
-const cors = require('cors');
+const server = require("http").createServer(app);
+const cors = require("cors");
 app.use(cors());
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-    },
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 const PORT = process.env.PORT || 5000;
-app.use(require('./router/auth'));
+app.use(require("./router/auth"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
-app.post('/hash', async (req, res) => {
-    const secPass = await bcrypt.hash(req.body.password, 10);
-    res.json({ secPass });
+app.post("/hash", async (req, res) => {
+  const secPass = await bcrypt.hash(req.body.password, 10);
+  res.json({ secPass });
 });
 
-app.post('/login', async (req, res) => {
-    const email = req.body.email;
-    const userPass = req.body.userPass;
-    const correctPass = req.body.correctPass;
-    const token = jwt.sign({ email }, 'jwtSecret', {
-        expiresIn: '10d',
-    });
-    if (await bcrypt.compare(userPass, correctPass)) {
-        res.json({ auth: true, token: token });
-    } else {
-        res.json({ auth: false });
-    }
+app.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const userPass = req.body.userPass;
+  const correctPass = req.body.correctPass;
+  const token = jwt.sign({ email }, "jwtSecret", {
+    expiresIn: "365d",
+  });
+  if (await bcrypt.compare(userPass, correctPass)) {
+    res.json({ auth: true, token: token });
+  } else {
+    res.json({ auth: false });
+  }
 });
 const users = {};
 
-io.on('connection', (socket) => {
-    console.log('ready to use');
-    socket.on('join-room', (roomId, email) => {
-        users[email] = true;
-        console.log('joined---', roomId, email);
-        socket.join(roomId);
-        // socket.to(roomId).emit('user-connected', users);
-        io.to(roomId).emit('user-connected', users);
+io.on("connection", (socket) => {
+  console.log("ready to use");
+  socket.on("join-room", (roomId, email) => {
+    users[email] = true;
+    console.log("joined---", roomId, email);
+    socket.join(roomId);
+    // socket.to(roomId).emit('user-connected', users);
+    io.to(roomId).emit("user-connected", users);
 
-        socket.on('disconnect', () => {
-            console.log('left---', email);
-            users[email] = false;
-            delete users[email];
-            // socket.to(roomId).emit('user-disconnected', users);
-            io.to(roomId).emit('user-disconnected', users);
-            // console.log(users);
-        });
+    socket.on("disconnect", () => {
+      console.log("left---", email);
+      users[email] = false;
+      delete users[email];
+      // socket.to(roomId).emit('user-disconnected', users);
+      io.to(roomId).emit("user-disconnected", users);
+      // console.log(users);
     });
+  });
 
-    socket.on('new-announcement', (roomId) => {
-        console.log('new announcement on server');
-        socket.join(roomId);
-        socket.broadcast.to(roomId).emit('show-announcement',"hello");
-    });
+  socket.on("new-announcement", (roomId) => {
+    console.log("new announcement on server");
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("show-announcement", "hello");
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`server running at http://127.0.0.1:${PORT}`);
+  console.log(`server running at http://127.0.0.1:${PORT}`);
 });
